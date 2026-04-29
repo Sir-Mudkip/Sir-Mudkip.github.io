dc193dfcef7d88d05af75a6534238a801e9e59f7
Sir-Mudkip <lukechappman@gmail.com>
2026-04-29 17:07:33 +0100

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sir-mudkip.github.io/',
+	site: 'https://blog.sir-mudkip.uk/',
 	integrations: [mdx(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
